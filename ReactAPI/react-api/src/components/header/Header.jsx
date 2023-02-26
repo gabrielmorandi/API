c49f99de2a83ed492d123499a7b18e2017c7aff2
Gabriel Morandi de Mello <61758357+gabrielmorandi@users.noreply.github.com>
2023-02-26 04:06:17 -0300

    add react-api frontend

---
+++ ReactAPI/react-api/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div class="container-flex">
+            <h1>Logo</h1>
+        </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ ReactAPI/react-api/src/config/ImageConfig.js
@@ -0,0 +1,20 @@
+const fileDefault = <i class="fa-sharp fa-regular fa-file"></i>
+const fileCode = <i class="fa-sharp fa-regular fa-file-code"></i>
+const fileImg = <i class="fa-sharp fa-regular fa-file-image"></i>
+const filePdf = <i class="fa-sharp fa-regular fa-file-pdf"></i>
+const fileRar = <i class="fa-sharp fa-regular fa-folder"></i>
+
+
+export const ImageConfig = {
+    default: fileDefault,
+    pdf: filePdf,
+    png: fileImg,
+    jpeg: fileImg,
+    html: fileCode,
+    css: fileCode,
+    js: fileCode,
+    json: fileCode,
+    sass: fileCode,
+    scss: fileCode,
+    'x-zip-compressed': fileRar
+}
